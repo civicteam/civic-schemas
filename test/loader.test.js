6630de26f1d988a996f3c4cf5a67505eb8bf9fa5
William Brooks <william@identity.org>
2021-09-10 12:23:21 +0200

    Tested loading with metadata

--- test/loader.test.js
+++ test/loader.test.js
@@ -1,5 +1,7 @@
+const { schemaLoader } = require('@identity.com/credential-commons');
 const { SchemaLoader } = require('../src');
 
+jest.setTimeout(50000);
 describe('Schema loader', () => {
   it('should load a valid schema', async () => {
     const loader = new SchemaLoader();
@@ -15,5 +17,25 @@ describe('Schema loader', () => {
     const schema = await loader.loadSchema('credential-cvc:Invalid-v1');
 
     expect(schema).toBeNull();
+  });
+
+  it('should load multiple schemas', async () => {
+    schemaLoader.addLoader(new SchemaLoader(undefined, null));
+
+    const initTime = new Date().getTime();
+    await schemaLoader.loadSchemaFromTitle('claim-cvc:Address.country-v1');
+    await schemaLoader.loadSchemaFromTitle('credential-cvc:Email-v1');
+    await schemaLoader.loadSchemaFromTitle('credential-cvc:GenericDocumentId-v1');
+    await schemaLoader.loadSchemaFromTitle('credential-cvc:IdDocument-v1');
+    await schemaLoader.loadSchemaFromTitle('credential-cvc:IdDocument-v2');
+    await schemaLoader.loadSchemaFromTitle('credential-cvc:Identity-v1');
+    await schemaLoader.loadSchemaFromTitle('credential-cvc:IDVaaS-v1');
+    await schemaLoader.loadSchemaFromTitle('credential-cvc:PhoneNumber-v1');
+    await schemaLoader.loadSchemaFromTitle('credential-cvc:UnverifiedAddress-v1');
+    await schemaLoader.loadSchemaFromTitle('credential-cvc:UnverifiedSsn-v1');
+
+    console.log(`Loaded ${schemaLoader.credentialDefinitions.length} credentials, 
+    ${schemaLoader.definitions.length} claims,
+     in ${(new Date().getTime() - initTime) / 1000} seconds`);
   });
 });
